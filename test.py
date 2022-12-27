9cc6abd55597992a0db5767b659d75ac5b9f22d5
jml51 <jose.lopes@ipvc.pt>
2022-12-27 10:11:33 +0000

    get page

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("hello word")
+from selenium import webdriver
+
+
+path = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(path)
+
+
+driver.get("https://www.pingodoce.pt/")
+
